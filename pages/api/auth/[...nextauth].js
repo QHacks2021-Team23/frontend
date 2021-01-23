800ab3b4211b1a813270d3c6826ad353ff4d4215
XavierChanth <xchanthavong@gmail.com>
2021-01-23 13:19:40 -0500

    add email and facebook provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,13 +4,36 @@ import Providers from "next-auth/providers";
 const options = {
   // Configure one or more authentication providers
   providers: [
+    Providers.Email({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    }),
+
     // ...add more providers here
   ],
-
+  pages: {
+    signIn: "/",
+  },
+  callbacks: {
+    redirect: async (url, baseUrl) => {
+      return Promise.resolve(baseUrl + "/editor");
+    },
+  },
   // A database is optional, but required to persist accounts in a database
   // database: process.env.DATABASE_URL,
 };
