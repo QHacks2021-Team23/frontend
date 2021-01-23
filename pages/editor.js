38c599bc68f823e0bca97fe3e9f8f5eeb33ddbdc
XavierChanth <xchanthavong@gmail.com>
2021-01-23 17:09:53 -0500

    formatting and offload toolbar consts

--- pages/editor.js
+++ pages/editor.js
@@ -10,6 +10,8 @@ import { EditorState } from "draft-js";
 
 import unescape from "lodash.unescape";
 
+import toolBarOptions from "../utils/toolbar";
+
 const DocumentEditor = dynamic(
   () => {
     return import("react-draft-wysiwyg").then((mod) => mod.Editor);
@@ -19,18 +21,26 @@ const DocumentEditor = dynamic(
 
 export default function Editor() {
   const [session, loading] = useSession();
-  const [showComments, setShowComments] = useState(false);
-  const [commentsState, setCommentsState] = useState(() =>
-    EditorState.createEmpty()
-  );
+
   const [editorState, setEditorStateState] = useState(() =>
     //TODO get state from backend
     // and update with use effect
     EditorState.createEmpty()
   );
   const [editorStateLength, setEditorStateLengthState] = useState(0);
+
+  const [showComments, setShowComments] = useState(false);
+
+  const [commentsState, setCommentsState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
   const toggleComments = () => {
     setShowComments(!showComments);
+  };
+
+  const refreshComments = async () => {
+    // TODO get comments from API
   };
 
   useEffect(() => {
@@ -54,21 +64,20 @@ export default function Editor() {
 
   const handleEditorChange = (e) => {
     setEditorState(e);
-    console.log(
-      e
-        .getCurrentContent()
-        .getBlocksAsArray()
-        .map((i) => i.getText())
-    );
+    // console.log(
+    //   e
+    //     .getCurrentContent()
+    //     .getBlocksAsArray()
+    //     .map((i) => i.getText())
+    // );
   };
 
   if (!loading && !session) {
-    console.log("redirect back");
     signin();
   }
 
   return (
-    <div className="center">
+    <div className="center bottom">
       {!session && (
         <div className="vertical-center">
           <Spin indicator={<LoadingOutlined style={{ fontSize: 52 }} spin />} />
@@ -76,15 +85,17 @@ export default function Editor() {
       )}
       {session && (
         <>
-          <h1>
+          <h1 className="session">
             {" "}
-            Signed in as {session.user.email} |{" "}
-            <Button onClick={signout}>Sign out</Button>
+            Signed in as {session.user.email}{" "}
+            <Button size="small" onClick={signout}>
+              Sign out
+            </Button>
           </h1>
-          <div>
-            <Button onClick={toggleComments}>Toggle Comments</Button>
-            <Button onClick={refreshComments}>Refresh Comments</Button>
-          </div>
+
+          <h1 className="title">
+            {showComments ? "Viewing Comments" : "Editing Essay"}
+          </h1>
 
           <div className="editor">
             <div className="editorWrapper">
@@ -93,7 +104,11 @@ export default function Editor() {
                 spellCheck={true}
                 editorState={editorState}
                 onEditorStateChange={handleEditorChange}
-                toolbar={toolbarOptions}
+                toolbar={toolBarOptions}
+                toolbarCustomButtons={[
+                  <Button onClick={toggleComments}>Toggle Comments</Button>,
+                  <Button onClick={refreshComments}>Refresh Comments</Button>,
+                ]}
               />
             </div>
           </div>
@@ -102,43 +117,3 @@ export default function Editor() {
     </div>
   );
 }
-
-const toolbarOptions = {
-  options: [
-    "blockType",
-    "fontSize",
-    "fontFamily",
-    "list",
-    "textAlign",
-    "history",
-  ],
-  blockType: {
-    inDropdown: true,
-    options: ["Normal", "H1", "H2", "H3", "H4", "H5", "H6"],
-  },
-  fontSize: {
-    options: [8, 9, 10, 11, 12, 14, 16, 18, 24, 30, 36, 48, 60, 72, 96],
-  },
-  fontFamily: {
-    options: [
-      "Arial",
-      "Georgia",
-      "Impact",
-      "Tahoma",
-      "Times New Roman",
-      "Verdana",
-    ],
-  },
-  list: {
-    inDropdown: false,
-    options: ["unordered", "ordered", "indent", "outdent"],
-  },
-  textAlign: {
-    inDropdown: false,
-    options: ["left", "center", "right", "justify"],
-  },
-  history: {
-    inDropdown: false,
-    options: ["undo", "redo"],
-  },
-};
